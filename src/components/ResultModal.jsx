456d7f1ca63f461ec6b9902e08302f637384aaca
Martin <matefortechnology@gmail.com>
2024-05-07 13:48:17 +0200

    Closing the modal via esc

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -10,7 +10,7 @@ const ResultModal = forwardRef(function ResultModal(
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
-  const score = Math.round((1 - remainingTime / (targetTime * 1000))*100);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -21,7 +21,7 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialogRef} className="result-modal">
+    <dialog ref={dialogRef} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2> Your Score: {score}</h2>}
       <p>
